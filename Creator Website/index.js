c43252cafc00a2e657e8fea72b2cb41089f86c55
Brian <brik71@gmail.com>
2022-04-01 16:46:41 -0400

    Multiple pieces and sides support, exporter and input for name and ability

--- Creator Website/index.js
+++ Creator Website/index.js
@@ -4,6 +4,8 @@
 // https://www.softouch.on.ca/svg/rotate1.html
 // https://stackoverflow.com/questions/9281199/adding-text-to-svg-document-in-javascript
 // https://codepen.io/remi-grumeau/pen/AwdRyM
+// https://stackoverflow.com/questions/9083037/convert-a-number-into-a-roman-numeral-in-javascript
+// https://medium.com/swlh/how-to-build-a-roman-numeral-to-integer-function-in-javascript-8298657a26f7
 
 // Lock Screen Scroll
 // Just for now
@@ -32,6 +34,10 @@ const movementIcons = ["move", "slide", "jump", "jumpSlide", "command"]
 // Indicated if the current piece is on the starting side or not
 let isStartSide = true;
 
+// Get SVG elements for the starting and non starting side icons
+let startSideIcon = document.getElementById("Starting-Side");
+let nonStartSideIcon = document.getElementById("Non-Starting-Side");
+
 // Get the SVG element for the entire template
 let svg = document.getElementById("svg");
 // When the mouse is over the template and the user scrolls, call mouseOnScroll()
@@ -46,15 +52,24 @@ let pieceIconLocation = document.getElementById("Piece-Icon")
 // Get the SVG element for where the player marker is
 let playerMarkerLocation = document.getElementById("Starting-Side")
 
+// ID number of current piece being worked on
+// Used so that multiple pieces can be worked on at a given time
+let currentPiece = 0;
+
+// Add event listener for key down that call the keyboard shortcut function
+svg.addEventListener('keydown', keyboardShortcuts);
+
+// Current version of the exporter, isn't needed currently
+// though I want to future proof everything just in case 
+let exporterVersion = 1;
+
+// Add event listeners for processing new name and ability inputs
+document.getElementById("nameInput").addEventListener('change', changeName);
+document.getElementById("abilityInput").addEventListener('change', changeAbility);
+
 // Initialization function
 function init() {
-  for(let i = 0; i < 5; i++){
-    let blankArray = []
-    for(let j = 0; j < 5; j++){
-      blankArray.push([])
-    }
-    iconList.push(blankArray)
-  }   
+  createNewPieceIndex()
 }
 
 function mouseOverGridSquare(item) {
@@ -84,7 +99,7 @@ function clickGridSquare(item) {
   item.style.fill="#ff0";
   if(iconElement){
     removeElement(gridPos[0], gridPos[1], iconElement.getAttribute("icon"), true)
-    iconList[gridPos[0]][gridPos[1]].push(iconElement)
+    iconList[currentPiece][+ isStartSide][gridPos[0]][gridPos[1]].push(iconElement)
     iconElement = null
   }
 }
@@ -99,7 +114,7 @@ function mouseOnScroll(event) {
     currentIcon = 0
   }
   if(currentIcon < 0) {
-    currentIcon = movementIcons.length-1
+    currentIcon = movementIcons.length - 1;
   }
 
   if(currentGridSquare) {
@@ -175,28 +190,20 @@ function getRotateDegrees(gridPos) {
   switch(gridPos.join()) {
     case "2,1":
       return "0"
-      break;
     case "3,1":
       return "45"
-      break;
     case "3,2":
       return "90"
-      break;
     case "3,3":
       return "135"
-      break;
     case "2,3":
       return "180"
-    break;
     case "1,3":
       return "225"
-      break;
     case "1,2":
       return "270"
-      break;
     case "1,1":
       return "315"
-      break;
   }
   return "0"
 }
@@ -204,24 +211,24 @@ function getRotateDegrees(gridPos) {
 // Remove all of the elements from view at the current grid location
 // removeFromList removes them from existance entirely
 function removeElement(x,y, newIcon, removeFromList = false) {
-  for(let i = 0; i < iconList[x][y].length; i++) {
-    if(isFullSize(iconList[x][y][i].getAttribute("icon")) && isFullSize(newIcon)) {
-      iconList[x][y][i].remove();
+  for(let i = 0; i < iconList[currentPiece][+ isStartSide][x][y].length; i++) {
+    if(isFullSize(iconList[currentPiece][+ isStartSide][x][y][i].getAttribute("icon")) && isFullSize(newIcon)) {
+      iconList[currentPiece][+ isStartSide][x][y][i].remove();
       if(removeFromList)
-        iconList[x][y].splice(i,1);
-    } else if(!isFullSize(iconList[x][y][i].getAttribute("icon")) && !isFullSize(newIcon)) {
-      iconList[x][y][i].remove();
+        iconList[currentPiece][+ isStartSide][x][y].splice(i,1);
+    } else if(!isFullSize(iconList[currentPiece][+ isStartSide][x][y][i].getAttribute("icon")) && !isFullSize(newIcon)) {
+      iconList[currentPiece][+ isStartSide][x][y][i].remove();
       if(removeFromList)
-        iconList[x][y].splice(i,1);
+        iconList[currentPiece][+ isStartSide][x][y].splice(i,1);
     }
   }
 }
 
 // Restores all elements into view at the current grid location
 function restoreElements(x,y) {
-  for(let i = 0; i < iconList[x][y].length; i++) {
-    iconList[x][y][i].remove();
-    svg.appendChild(iconList[x][y][i])
+  for(let i = 0; i < iconList[currentPiece][+ isStartSide][x][y].length; i++) {
+    iconList[currentPiece][+ isStartSide][x][y][i].remove();
+    svg.appendChild(iconList[currentPiece][+ isStartSide][x][y][i])
   }
 }
 
@@ -229,18 +236,18 @@ function restoreElements(x,y) {
 // only elements that are of the opposite icon type of that given.
 // Used for when using scrolling to show icons
 function restoreOppositeType(x,y,newIcon) {
-  for(let i = 0; i < iconList[x][y].length; i++) {
-    if(isFullSize(iconList[x][y][i].getAttribute("icon")) && !isFullSize(newIcon)) {
-      iconList[x][y][i].remove();
-      svg.appendChild(iconList[x][y][i])
-    } else if(!isFullSize(iconList[x][y][i].getAttribute("icon")) && isFullSize(newIcon)) {
-      iconList[x][y][i].remove();
-      svg.appendChild(iconList[x][y][i])
-
+  for(let i = 0; i < iconList[currentPiece][+ isStartSide][x][y].length; i++) {
+    if(isFullSize(iconList[currentPiece][+ isStartSide][x][y][i].getAttribute("icon")) && !isFullSize(newIcon)) {
+      iconList[currentPiece][+ isStartSide][x][y][i].remove();
+      svg.appendChild(iconList[currentPiece][+ isStartSide][x][y][i])
+    } else if(!isFullSize(iconList[currentPiece][+ isStartSide][x][y][i].getAttribute("icon")) && isFullSize(newIcon)) {
+      iconList[currentPiece][+ isStartSide][x][y][i].remove();
+      svg.appendChild(iconList[currentPiece][+ isStartSide][x][y][i])
     }
   }
 }
 
+// Creates a text element at the position given of the font size given
 function createTextAt(text, pos, fontSize) {
   let svgNS = "http://www.w3.org/2000/svg";
   let newText = document.createElementNS(svgNS,"text");
@@ -251,31 +258,39 @@ function createTextAt(text, pos, fontSize) {
   newText.setAttribute("x",cx);
   newText.setAttribute("y",cy);
   newText.setAttribute("font-family", "Pieces of Eight")
+  newText.setAttribute("text", text)
   svg.appendChild(newText);
   let bbox = newText.getBBox()
   cx -= bbox.width/2
   cy += bbox.height/4
   newText.setAttribute("x",cx);
   newText.setAttribute("y",cy);
-
+  return newText;
 }
 
+// Create a text element of the given piece name
 function createPieceName(text) {
+  if(iconList[currentPiece][2][0])
+    iconList[currentPiece][2][0].remove()
   let namePos = getCenter(nameLocation);
-  createTextAt(text, namePos, 270);
+  iconList[currentPiece][2][0] = createTextAt(text, namePos, 270);
 }
 
+// Create a text element of the given ability number
 function createPieceAbilityText(text) {
+  if(iconList[currentPiece][2][1])
+    iconList[currentPiece][2][1].remove()
   let iconPos = getCenter(pieceIconLocation);
   let namePos = getCenter(nameLocation);
   iconPos.y = namePos.y + 140;
-  createTextAt(text, iconPos, 100);
+  iconList[currentPiece][2][1] = createTextAt(text, iconPos, 100);
 }
 
+// Create the piece icon of the given piece name
 function createPieceIcon(piece) {
   let iconPos = getCenter(pieceIconLocation);
   let pieceIconElement = document.getElementById(piece).cloneNode(true);
-  let width = 375;
+  let width = 370;
   let cx = iconPos.x - (width/2) + 15;
   let cy = iconPos.y;
   pieceIconElement.setAttribute("x", cx);
@@ -285,8 +300,225 @@ function createPieceIcon(piece) {
   svg.appendChild(pieceIconElement);
 }
 
-init()
+// Create a new index in iconList for a new piece to be placed in
+function createNewPieceIndex() {
+  let masterArray = []
+  for(let i = 0; i < 2; i++) {
+    let blankParentArray = []
+    for(let j = 0; j < 5; j++){
+      let blankArray = []
+      for(let k = 0; k < 5; k++){
+        blankArray.push([])
+      }
+      blankParentArray.push(blankArray)
+    } 
+    masterArray.push(blankParentArray)
+  } 
+  let infoArray = [];
+  for(let i = 0; i < 3; i++) {
+    infoArray.push("");
+  }
+  masterArray.push(infoArray);
+  iconList.push(masterArray); 
+}
 
-createPieceName("Duke")
-//createPieceAbilityText("III")
+// Clear all of the icons for the current piece and side from view
+function clearBoard() {
+  for(let i = 0; i < iconList[currentPiece][+ isStartSide].length; i++) {
+    for(let j = 0; j < iconList[currentPiece][+ isStartSide][i].length; j++){
+      for(let k = 0; k < iconList[currentPiece][+ isStartSide][i][j].length; k++) {
+        iconList[currentPiece][+ isStartSide][i][j][k].remove();
+      }
+    }
+  }
+}
+
+// Redraw all of the icons for the current piece and side from view
+function drawBoard() {
+  for(let i = 0; i < iconList[currentPiece][+ isStartSide].length; i++) {
+    for(let j = 0; j < iconList[currentPiece][+ isStartSide][i].length; j++){
+      for(let k = 0; k < iconList[currentPiece][+ isStartSide][i][j].length; k++) {
+        svg.appendChild(iconList[currentPiece][+ isStartSide][i][j][k]);
+      }
+    }
+  }
+}
+
+// Clears all elements that are not on the grid (name, ability icon)
+function clearNonBoard() {
+  if(iconList[currentPiece][2][0])
+    iconList[currentPiece][2][0].remove()
+  if(iconList[currentPiece][2][1])
+    iconList[currentPiece][2][1].remove()
+}
+
+// Draw all elements that are not on the grid (name, ability icon)
+function drawNonBoard() {
+  if(iconList[currentPiece][2][0])
+    svg.appendChild(iconList[currentPiece][2][0])
+  if(iconList[currentPiece][2][1])
+    svg.appendChild(iconList[currentPiece][2][1])
+}
+
+// Easy macro to create a new piece index and increment the current piece
+function createNewPiece() {
+  createNewPieceIndex();
+  forwardPiece();
+  console.log(iconList)
+}
+
+// Easy macro for switching the side of the current piece
+function switchSides() {
+  clearBoard()
+  isStartSide = !isStartSide;
+  showStartSideIcons()
+  drawBoard();
+}
+
+// Easy macro for changing the visability of the start and non start side icons
+function showStartSideIcons() {
+  startSideIcon.setAttribute("visibility", isStartSide ? "visable" : "hidden")
+  nonStartSideIcon.setAttribute("visibility", !isStartSide ? "visable" : "hidden")
+}
+
+// Move forward one piece in the piece array 
+function forwardPiece() {
+  clearBoard();
+  clearNonBoard()
+  currentPiece++;
+  if(currentPiece >= iconList.length)
+    currentPiece = 0;
+  isStartSide = true;
+  showStartSideIcons()
+  drawNonBoard()
+  drawBoard();
+}
+
+// Move backwards one piece in the piece array 
+function backwardPiece() {
+  clearBoard();
+  clearNonBoard()
+  currentPiece--;
+  if(currentPiece < 0)
+    currentPiece = iconList.length - 1;
+  isStartSide = true;
+  showStartSideIcons();
+  drawNonBoard()
+  drawBoard();
+}
+
+// Process keyboard inputs 
+function keyboardShortcuts(e) {
+  switch(e.code){
+    case "KeyZ":
+      backwardPiece()
+      break;
+    case "KeyX":
+      forwardPiece()
+      break;
+    case "KeyN":
+      createNewPiece()
+      break;
+    case "KeyS":
+      switchSides()
+      break;
+    case "KeyE":
+      exportPieces()
+      break;
+  }
+}
+
+function exportPieces() {
+  let object = {}
+  object.options = {}
+  object.options.exporterVersion = exporterVersion;
+  
+  object.pieces = []
+  for(let i = 0; i < iconList.length; i++) {
+    let pieceObject = {};
+    let iconArray = [];
+    for(let j = 0; j < 2; j++) {
+      let sideArray = [];
+      for(let x = 0; x < iconList[i][j].length; x++) {
+        let xArray = [];
+        for(let y = 0; y < iconList[i][j][x].length; y++) {
+          let yArray = [];
+          for(let k = 0; k < iconList[i][j][x][y].length; k++) {
+            yArray.push(iconList[i][j][x][y][k].getAttribute("icon"))
+          }
+          xArray.push(yArray);
+        }
+        sideArray.push(xArray);
+      }
+      iconArray.push(sideArray);
+    }
+    pieceObject.grid.startSide = iconArray[1];
+    pieceObject.grid.startNonSide = iconArray[0];
+    pieceObject.name = iconList[i][2][0].getAttribute("text");
+    pieceObject.ability = romanToInt(iconList[i][2][1].getAttribute("text"));
+    object.pieces.push(pieceObject);
+  }
+}
+
+function romanize (num) {
+  if (isNaN(num))
+      return NaN;
+  var digits = String(+num).split(""),
+      key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
+             "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+             "","I","II","III","IV","V","VI","VII","VIII","IX"],
+      roman = "",
+      i = 3;
+  while (i--)
+      roman = (key[+digits.pop() + (i * 10)] || "") + roman;
+  return Array(+digits.join("") + 1).join("M") + roman;
+}
+
+const romanHash = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+const s = "MCMLXXXIX";
+// s = 1989
+function romanToInt(s) {
+  let accumulator = 0;for (let i = 0; i < s.length; i++) {
+    if (s[i] === "I" && s[i + 1] === "V") {
+      accumulator += 4;
+      i++;
+    } else if (s[i] === "I" && s[i + 1] === "X") {
+      accumulator += 9;
+      i++;
+    } else if (s[i] === "X" && s[i + 1] === "L") {
+      accumulator += 40;
+      i++;
+    } else if (s[i] === "X" && s[i + 1] === "C") {
+      accumulator += 90;
+      i++;
+    } else if (s[i] === "C" && s[i + 1] === "D") {
+      accumulator += 400;
+      i++;
+    } else if (s[i] === "C" && s[i + 1] === "M") {
+      accumulator += 900;
+      i++;
+    } else {
+      accumulator += romanHash[s[i]];
+    }
+  }
+  return accumulator;
+}
+
+function changeName(e) {
+  createPieceName(e.target.value);
+}
+
+function changeAbility(e) {
+  createPieceAbilityText(romanize(e.target.value));
+}
+
+init()
 createPieceIcon("dukeIcon")
