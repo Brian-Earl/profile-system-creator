6ccd9bc6bc5fa45e76135bb5673b2cbd0c11929b
Brian <brik71@gmail.com>
2022-05-11 15:34:31 -0400

    Rearrange file structure

--- index.js
+++ index.js
@@ -29,6 +29,7 @@
 // Revamp UI
 // Full size piece support
 // Redo Icons (look at Start Side Icon Affinity File)
+// Store what peices (if any) a piece should replace
 
 // Variable that keeps track all of the current pieces created
 // An array of arrays, each array contains two arrays, both are 2D arrays
@@ -1711,19 +1712,9 @@ function splitAtFirstOccurance(text, split) {
 
 // Reverse a string
 function reverseString(str) {
-  // Step 1. Use the split() method to return a new array
-  var splitString = str.split(""); // var splitString = "hello".split("");
-  // ["h", "e", "l", "l", "o"]
-
-  // Step 2. Use the reverse() method to reverse the new created array
-  var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
-  // ["o", "l", "l", "e", "h"]
-
-  // Step 3. Use the join() method to join all elements of the array into a string
-  var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
-  // "olleh"
-
-  //Step 4. Return the reversed string
+  let splitString = str.split(""); 
+  let reverseArray = splitString.reverse(); 
+  let joinArray = reverseArray.join("");
   return joinArray; // "olleh"
 }
 
